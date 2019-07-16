50aed78f6248439a5e44cc52acba657f0aca68d4
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-07-16 11:31:40 +0100

    if statements about weather

--- 08control_flow.py
+++ 08control_flow.py
@@ -15,11 +15,21 @@
 # else:
 #     print('you are underage, you cannot vote')
 
-weather = input('Is it rainy or sunny today?').strip().lower()
+# weather = input('Is it rainy or sunny today?').strip().lower()
+#
+# if weather == 'rainy':
+#     print('Take an umbrella with you today')
+# elif weather == 'sunny':
+#     print('Wear a hat')
+# else:
+#     print('Stay home today')
 
-if weather == 'rainy':
-    print('Take an umbrella with you today')
-elif weather == 'sunny':
-    print('Wear a hat')
+weather1 = input('What is the weather like today?').strip().lower()
+weather2 = input('Anything else?').strip().lower()
+
+if (weather1 == 'rainy' and weather2 == 'stormy') or (weather2 == 'stormy' and weather1 == 'rainy'):
+    print('Take your jacket')
+elif (weather1 == 'rainy' and weather2 == 'no') or (weather1 == 'foggy' and weather2 == 'no'):
+    print('Take your umbrella')
 else:
-    print('Stay home today')
+    print('Your answer can not be recongnised, please try again')
