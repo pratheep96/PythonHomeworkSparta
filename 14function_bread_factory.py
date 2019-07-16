d880b8c53a7f99a4b6c94d7ca956641eef0e3226
Pratheep Premaraj <PPremaraj@spartaglobal.com>
2019-07-16 16:55:49 +0100

    bread factory

---
+++ 14function_bread_factory.py
@@ -0,0 +1,26 @@
+# Our Bread Factory
+
+def make_dough(ingridient1, ingridient2):
+    if 'wheat' in ingridient1 and 'water' in ingridient2:
+        return 'dough'
+    else:
+        return 'wrong ingridients'
+
+def bake_bread(semi_product):
+    if semi_product == 'dough':
+        return 'bread'
+    else:
+        return 'wrong ingridients'
+
+#Tests
+print('call function make_dough, expect dough to be returned')
+print(make_dough('wheat', 'water')  == 'dough')
+
+print('call function make_dough with wrong ingridients. Expect it to return wrong ingridients')
+print(make_dough('cement', 'water') == 'wrong ingridients')
+
+print('call function bake_bread, expect bread to be returned')
+print(bake_bread('dough') == 'bread')
+
+print('called function bake_bread wiht wrong ingridients. expect outcome to be wrong ingridients')
+print(bake_bread('chocolate cement') == 'wrong ingridients')
