f0f584d0b33efd64144d0c292a31275cc3c7b31e
pratheep96 <pratheep96@hotmail.com>
2019-07-22 09:27:48 +0100

    Add files via upload

---
+++ Airport.py
@@ -0,0 +1,27 @@
+Airport
+
+As a checkin - assistant I can create a passenger
+
+As a checkin - assistant I can list existing flights
+
+As
+a
+checkin - asistant
+I
+can
+add
+a
+passenger
+to
+a
+flight
+
+People --> Passengers - Name
+- Passport
+Airport --> Aircraft - Airplanes
+- Helicopter
+
+Flight --> self.plane
+self.passengers_list
+self.destination
+self.origin
